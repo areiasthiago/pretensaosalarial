d85221a7cccc81d0ba823d00ea912b8978d38c2a
Thiago Areias <thiagoareias@gmail.com>
2026-04-21 15:03:46 -0300

    fix: corrige fetch do package.json para funcionar local e no GitHub Pages

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 async function loadVersion() {
   try {
-    const res = await fetch('/pretensaosalarial/package.json')
+    const res = await fetch('package.json')
     const pkg = await res.json()
     const badges = document.querySelectorAll('.version-tag')
     badges.forEach(el => el.textContent = `beta v${pkg.version}`)
