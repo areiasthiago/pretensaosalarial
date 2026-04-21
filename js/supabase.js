c8b9e8ef8581025ccf3e5e2ce2eff7d3bb3f0394
Thiago Areias <thiagoareias@gmail.com>
2026-04-21 14:02:00 -0300

    setup inicial: estrutura de arquivos e conexão supabase

---
+++ js/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://mzbczjzmeuadyysexubj.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable_gGeTDDawtLMyUrphyX8JPA_hM2Ug0V_'
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
